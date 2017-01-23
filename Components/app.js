5283436579a1b1a2eb13bfaf5607437042ac746f
rodk <rod2010rod2010@hotmail.com>
2017-01-22 19:58:36 -0500

    created additional Showlist2.js

--- Components/app.js
+++ Components/app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import AddFriend from './addfriend.js'
 import ShowList from './showlist.js'
+import AddFriend2 from './addfriend2.js'
+import ShowList2 from './showlist2.js'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,12 +10,20 @@ export default class App extends React.Component {
     this.state = {
       note: this.props.note,
       name: this.props.name[0]+ " " + this.props.name[1],
-      friends: this.props.friends
+      friends: this.props.friends,
+      note2: this.props.note2,
+      name2: this.props.name2[0]+ " " + this.props.name2[1],
+      friends2: this.props.friends2
     }
   }
   addFriend(friend) {
     this.setState({
       friends: this.state.friends.concat([friend])
+    });
+  }
+  addFriend2(friend) {
+    this.setState({
+      friends2: this.state.friends2.concat([friend])
     });
   }
   render() {
@@ -40,6 +50,11 @@ export default class App extends React.Component {
         <h2 style={myStyle3}>Name: &nbsp;&nbsp;{this.state.name}</h2>
         <AddFriend addNew={this.addFriend.bind(this)} />
         <ShowList names={this.state.friends} />
+
+        <h1 style={myStyle2}>{this.state.note2}</h1>
+        <h2 style={myStyle3}>Name: &nbsp;&nbsp;{this.state.name2}</h2>
+        <AddFriend2 addNew={this.addFriend2.bind(this)} />
+        <ShowList2 names={this.state.friends2} />
       </div>
     );
   }
@@ -54,10 +69,24 @@ App.defaultProps = {
     'Gottfried von Leibniz',
     'Anonymous Friend',
     'Big Fan'
+  ],
+  note2: "Another Example.",
+  name2: ['Albert', 'Einstein'],
+  friends2: [
+    'Batman',
+    'Superman',
+    'Wonderwoman',
+    'Leonardo',
+    'MichaelAngelo',
+    'Raphael',
+    'Donatello'
   ]
 }
 App.propTypes = {
   note: React.PropTypes.string,
   name: React.PropTypes.array.isRequired,
-  friends: React.PropTypes.array.isRequired
+  friends: React.PropTypes.array.isRequired,
+  note2: React.PropTypes.string,
+  name2: React.PropTypes.array.isRequired,
+  friends2: React.PropTypes.array.isRequired
 }
---
+++ Components/showlist2.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default class ShowList2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Friends</h2>
+        <ul>
+        {this.props.names.map(item => 
+          <li key={item}>{item}</li>
+        )}
+        </ul>
+      </div>
+    );
+  }
+}
+ShowList2.defaultProps = {
+  names: []
+}
+ShowList2.propTypes = {
+  names: React.PropTypes.array.isRequired
+}
