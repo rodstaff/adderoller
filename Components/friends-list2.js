fff73c7498911ed43d59a929140f1b401c5da6f5
rodk <rod2010rod2010@hotmail.com>
2017-01-24 16:33:46 -0500

    refactored Components/friends-list2.js

--- Components/friends-list2.js
+++ Components/friends-list2.js
@@ -6,14 +6,14 @@ export default class FriendsList2 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      note2: this.props.note2,
-      name2: this.props.name2[0]+ " " + this.props.name2[1],
-      friends2: this.props.friends2
+      note: this.props.note,
+      name: this.props.name[0]+ " " + this.props.name[1],
+      friends: this.props.friends
     }
   }
-  addFriend2(friend) {
+  addFriend(friend) {
     this.setState({
-      friends2: this.state.friends2.concat([friend])
+      friends: this.state.friends.concat([friend])
     });
   }
   render() {
@@ -36,18 +36,18 @@ export default class FriendsList2 extends React.Component {
     }
     return (
       <div style={myStyle1}>
-        <h1 style={myStyle2}>{this.state.note2}</h1>
-        <h2 style={myStyle3}>Name: &nbsp;&nbsp;{this.state.name2}</h2>
-        <AddFriend2 addNew={this.addFriend2.bind(this)} />
-        <ShowList2 names={this.state.friends2} />
+        <h1 style={myStyle2}>{this.state.note}</h1>
+        <h2 style={myStyle3}>Name: &nbsp;&nbsp;{this.state.name}</h2>
+        <AddFriend2 addNew={this.addFriend.bind(this)} />
+        <ShowList2 names={this.state.friends} />
       </div>
     );
   }
 }
 FriendsList2.defaultProps = {
-  note2: "Another Example.",
-  name2: ['Albert', 'Einstein'],
-  friends2: [
+  note: "Another Example.",
+  name: ['Albert', 'Einstein'],
+  friends: [
     'Batman',
     'Superman',
     'Wonderwoman',
@@ -58,7 +58,69 @@ FriendsList2.defaultProps = {
   ]
 }
 FriendsList2.propTypes = {
-  note2: React.PropTypes.string,
-  name2: React.PropTypes.array.isRequired,
-  friends2: React.PropTypes.array.isRequired
+  note: React.PropTypes.string,
+  name: React.PropTypes.array.isRequired,
+  friends: React.PropTypes.array.isRequired
 }
+
+
+// export default class FriendsList2 extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       note2: this.props.note2,
+//       name2: this.props.name2[0]+ " " + this.props.name2[1],
+//       friends2: this.props.friends2
+//     }
+//   }
+//   addFriend2(friend) {
+//     this.setState({
+//       friends2: this.state.friends2.concat([friend])
+//     });
+//   }
+//   render() {
+//     const myStyle1 = {
+//       borderStyle:  "solid",
+//       borderWidth: 5,   //outside <div>
+//       marginLeft: 150,
+//       marginRight: 100,
+//       marginTop: 50,
+//       paddingLeft: 20,  //inside <div>
+//       paddingTop: 5
+//     }
+//     const myStyle2 = {
+//       color: "indigo",
+//       fontSize: 50
+//     }
+//     const myStyle3 = {
+//       color: "brown",
+//       fontSize: 40
+//     }
+//     return (
+//       <div style={myStyle1}>
+//         <h1 style={myStyle2}>{this.state.note2}</h1>
+//         <h2 style={myStyle3}>Name: &nbsp;&nbsp;{this.state.name2}</h2>
+//         <AddFriend2 addNew={this.addFriend2.bind(this)} />
+//         <ShowList2 names={this.state.friends2} />
+//       </div>
+//     );
+//   }
+// }
+// FriendsList2.defaultProps = {
+//   note2: "Another Example.",
+//   name2: ['Albert', 'Einstein'],
+//   friends2: [
+//     'Batman',
+//     'Superman',
+//     'Wonderwoman',
+//     'Leonardo',
+//     'MichaelAngelo',
+//     'Raphael',
+//     'Donatello'
+//   ]
+// }
+// FriendsList2.propTypes = {
+//   note2: React.PropTypes.string,
+//   name2: React.PropTypes.array.isRequired,
+//   friends2: React.PropTypes.array.isRequired
+// }
