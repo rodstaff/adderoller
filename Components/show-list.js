a565c977e4258d503eec87d52d2452e6a27b7059
rodk <rod2010rod2010@hotmail.com>
2017-02-17 23:26:29 -0500

    modified Showlist.js

--- Components/show-list.js
+++ Components/show-list.js
@@ -4,7 +4,7 @@ export default class ShowList extends React.Component {
   render() {
     return (
       <div>
-        <h2>Friends</h2>
+        <h2>Friends List</h2>
         <ul>
         {this.props.names.map(item => 
           <li key={item}>{item}</li>
@@ -20,17 +20,3 @@ ShowList.defaultProps = {
 ShowList.propTypes = {
   names: React.PropTypes.array.isRequired
 }
-
-// class ShowList extends React.Component {
-//   render() {
-//     var listItems = this.props.names.map((item) => {
-//       return <li key={item}>{item}</li>
-//     })
-//     return (
-//       <div>
-//         <h3>Friends</h3>
-//         <ul>{listItems}</ul>
-//       </div>
-//     );
-//   }
-// }
